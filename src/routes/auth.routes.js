1a9cc11df4258c7e526d17687e5185a391020908
Julio Aguilar <cuentas.96@live.com>
2021-11-26 12:36:40 -0300

    some requests require a token 8) get/id don't require || post put delete require token

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,8 +1,9 @@
+//imports
 import Router from "express";
 import * as authController from '../controllers/authsController';
-
+//constants
 const router = Router();
-
+//routes
 router.post('/signup', authController.signUp);
 router.post('/signin', authController.signIn);
 
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,14 +1,15 @@
 ////imports
 import { Router } from "express";
 import * as productController from '../controllers/productsController';
+import { authJwt } from '../middlewares';
 ////constants
 const router = Router();
-
-router.post('/', productController.createProduct); // now here, depending of the verb http the function is called
+//routes
 router.get('/', productController.getProducts); // go to controllers/products
 router.get('/:id', productController.getProductById);
-router.put('/:id', productController.updateProduct);
-router.delete('/:id', productController.deleteProducts);
+router.post('/', [authJwt.verifyToken, authJwt.isModerator], productController.createProduct); // now here, depending of the verb http the function is called
+router.put('/:id', [authJwt.verifyToken, authJwt.isAdmin], productController.updateProduct);
+router.delete('/:id', [authJwt.verifyToken, authJwt.isAdmin], productController.deleteProducts);
 
 ////export to app.js
 export default router;
