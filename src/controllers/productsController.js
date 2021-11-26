17e260a81828038cde77aa4c69c8bf0733a16ca9
Julio Aguilar <cuentas.96@live.com>
2021-11-26 04:41:09 -0300

    now we can use all the verbs http :)

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -2,7 +2,7 @@ import Product from '../models/product';
 
 export const createProduct = async (req, res) => {
     const { name, category, price, imgURL } = req.body; //the request is from the body, not from the query /:params
-    const newProduct = new Product({ name, category, price, imgURL }); // must be a object
+    const newProduct = new Product({ name, category, price, imgURL }); // must be a object ({})
     const productSaved = await newProduct.save(); //at the same time you save the object, assign it to a const
     console.log(productSaved);
     res.status(201).json(productSaved);//now you can show the object saved to the client
@@ -13,14 +13,21 @@ export const getProducts = async (req, res) => {
     res.json(products);
 }
 
-export const getProductById = (req, res) => {
-
+export const getProductById = async (req, res) => {
+    const id = req.params.id;
+    const product = await Product.findById(id);
+    res.json(product);
 }
 
-export const updateProduct = (req, res) => {
-
+export const updateProduct = async (req, res) => {
+    const id = req.params.id;
+    const content = req.body;
+    const product = await Product.findByIdAndUpdate(id, content, { new: true });
+    res.json(product)
 }
 
-export const deleteProducts = (req, res) => {
-
+export const deleteProducts = async (req, res) => {
+    const id = req.params.id;
+    const product = await Product.findByIdAndDelete(id);
+    res.json(product);
 }
